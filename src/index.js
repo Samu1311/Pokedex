a9c9241a6f1b006d3ee1f0e9b01ab1b16ba6a175
Samu1311 <samuele13112002@gmail.com>
2024-04-20 12:27:57 +0200

    Update React imports and Router in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Homepage from './Components/Main';
-import Legends from './Components/Legends';
-import About from './Components/About';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Homepage from "./Components/Main";
+import Legends from "./Components/Legends";
+import About from "./Components/About";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,5 +16,5 @@ ReactDOM.render(
       </Routes>
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
